bcda9798a95026e525134a03bc23c3e0df8e31b7
Nikhil Makkad <nikmakkad08@gmail.com>
2019-06-14 15:46:35 +0530

    find sum of all subarrays in o(n) time

---
+++ subarray2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+long int SubArraySum( int arr[] , int n )
+{
+    long int result = 0;
+
+    for (int i=0; i<n; i++)
+        result += (arr[i] * (i+1) * (n-i));
+
+    return result ;
+}
+int main()
+{
+   int arr[] = {1, 2, 3} ;
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout << "Sum of SubArray : "
+         << SubArraySum(arr, n) << endl;
+    return 0;
+}
